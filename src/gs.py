ac9c4b99d92a3ebeaacada4c9c74a04e1e1ecf66
ragheedalhyder <ragheedalhyder@gmail.com>
2023-12-14 19:50:43 +0100

    groundstate done

--- src/gs.py
+++ src/gs.py
@@ -1,55 +1,60 @@
-# Groud state properties, calculate c_n(0) and omega_0
+# A class that includes all the groundstate functions
 
 import numpy as np
+class groundstate:
+    def __init__(self, max_iter, N, dJU, muU):
+        self.max_iter = max_iter
+        self.N = N
+        self.dJU = dJU
+        self.muU = muU
 
+    def __str__(self):
+        return f"max_iter = {self.max_iter}, N = {self.N}, dJU = {self.dJU}, muU = {self.muU}, psi0 = {self.psi0}, n0 = {self.n0}, omega0U = {self.omega0U}"
 
-def delta(x, y):
-    return np.where(x == y, 1, 0)
+    def __repr__(self):
+        return f"max_iter = {self.max_iter}, N = {self.N}, dJU = {self.dJU}, muU = {self.muU}, psi0 = {self.psi0}, n0 = {self.n0}, omega0U = {self.omega0U}"
+    
+    def delta(self, x, y):
+        return np.where(x == y, 1, 0)
+    
+    def psi0(self, cns):
+        N = self.N
+        i = np.arange(1, N)
+        psi0 = np.sum(np.sqrt(i) * cns[i - 1] * cns[i])
+        return psi0
+    
+    def n0(self, cns):
+        i = np.arange(0, self.N)
+        return np.sum(i * cns * cns)
+    
+    def omega0U(self, cns):
+        ns = np.arange(0, self.N, 1)
+        psi0 = self.psi0(cns, self.N)
+        return - 2 * self.dJU * psi0 * psi0 + np.sum( ( 0.5 * ns * (ns - 1) - self.muU * ns)  * cns * cns)
+    
+    def cns(self):
+        N = self.N
+        dJU = self.dJU
+        muU = self.muU
+        max_iter = self.max_iter
 
+        mat = np.zeros((N, N))
+        psi0 = 1
+        i = np.arange(N)
+        j = np.arange(N)
 
-def psi0(cns, N):
-    i = np.arange(1, N)
-    return np.sum(np.sqrt(i) * cns[i - 1] * cns[i])
+        for k in range(max_iter):
+            i_matrix, j_matrix = np.meshgrid(i, j)
+            mat = (
+                (0.5 * i_matrix * (i_matrix - 1) - muU * i_matrix)
+                * self.delta(i_matrix, j_matrix)
+                - dJU * np.sqrt(i_matrix) * psi0 * self.delta(i_matrix, j_matrix + 1)
+                - dJU * np.sqrt(i_matrix + 1) * psi0 * self.delta(i_matrix + 1, j_matrix) )
 
-
-def cns(max_iter, N, dJU, muU):
-    mat = np.zeros((N, N))
-    psi0 = 1
-    i = np.arange(N)
-    j = np.arange(N)
-
-    for k in range(max_iter):
-        i_matrix, j_matrix = np.meshgrid(i, j)
-        mat = (
-            (0.5 * i_matrix * (i_matrix - 1) - muU * i_matrix)
-            * delta(i_matrix, j_matrix)
-            - dJU * np.sqrt(i_matrix) * psi0 * delta(i_matrix, j_matrix + 1)
-            - dJU * np.sqrt(i_matrix + 1) * psi0 * delta(i_matrix + 1, j_matrix)
-        )
-
-        Eigvals, Eigvecs = np.linalg.eig(mat)
-        ind = np.argmin(Eigvals)
-        cns = np.abs(np.real(Eigvecs[:, ind]))
-
-        psi0 = psi0(cns, N)
-
-        # omega0U = 0
-        # ns = np.arange(0, N, 1)
-        # n0 =  np.sum(ns * cns * cns)
-        # omega0U = np.sum( ( 0.5 * ns * (ns - 1) - muU * ns)  * cns * cns)
-
-        # n0s[count] = n0
-        # omega0U = - 2 * dJU * psi0 * psi0 + omega0U
-    print("dJU = ", dJU, "psi0 = ", psi0, np.size(mat))
-    return cns
-
-
-def omega0U(max_iter, N, dJU, muU):
-    cns = cns(max_iter, N, dJU, muU)
-    psi0 = psi0(cns, N)
-    ns = np.arange(0, N, 1)
-    n0 = np.sum(ns * cns * cns)
-    omega0U = -2 * dJU * psi0 * psi0 + np.sum(
-        (0.5 * ns * (ns - 1) - muU * ns) * cns * cns
-    )
-    return omega0U
+            Eigvals, Eigvecs = np.linalg.eig(mat)
+            ind = np.argmin(Eigvals)
+            cns = np.abs(np.real(Eigvecs[:, ind]))
+            psi0 = self.psi0(cns)
+    
+        return cns
+    
--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ import numpy as np
 import os
 import utils
 from grid import Grid
-from cns import psi
+from gs import groundstate
 from plotting import plot2D, plot_cns
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -13,21 +14,35 @@ def main():
 
     config = utils.read_config(args.config)
 
-    grid = Grid(config["grid"]["Lx"], config["grid"]["Ly"])
-
+    Lx = config["grid"]["Lx"]
+    Ly = config["grid"]["Ly"]
+    max_iter = config["max_iter_psi"]
     dJUs = np.arange(**config["physics"]["dJU"])
-    print(dJUs)
-    cnslist = []
-    for dJU in dJUs:
-        cns = psi(
-            config["max_iter_psi"],
-            config["grid"]["N"],
-            0.5,
-            eval(config["physics"]["Mu"]),
-        )
-        cnslist.append(cns)
+    N = config["grid"]["N"]
+    muU = eval(config["physics"]["Mu"])
+    grid = Grid(Lx, Ly)
 
-    plot_cns(cnslist, r"$n$", r"$\bar{c}_n$", os.path.join(output_dir, "csn.png"))
+    gs = groundstate(max_iter, N, dJUs[10], muU)
+    
+    cns = gs.cns()
+    omega0U = gs.omega0U(cns)
+    psi0 = gs.psi0(cns)
+    n0 = gs.n0(cns)
+    
+    plt.plot(cns)
+    plt.xlim(0, 70)
+    plt.show()
+    # psi0 = psi0(cns, N)
+    # n0 = n0(cns, N)
+    # omega0U = omega0(cns, N, dJU, muU, psi0)
+
+    # n0s[count] = n0
+    # omega0U = - 2 * dJU * psi0 * psi0 + omega0U
+    # for dJU in dJUs:
+    #     cns = psi(config["max_iter_psi"], config["grid"]["N"], 0.5, eval(config["physics"]["Mu"]))
+    #     cnslist.append(cns)
+
+    # plot_cns(cnslist, r'$n$', r'$\bar{c}_n$', os.path.join(output_dir, "csn.png") )
 
 
 if __name__ == "__main__":
