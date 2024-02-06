37b6324e09db08ba2907edad0ba4f83d9923e7af
ragheedalhyder <ragheedalhyder@gmail.com>
2024-02-06 13:33:24 +0100

    Vectorized T matrix

--- src/t_mat.py
+++ src/t_mat.py
@@ -83,89 +83,33 @@ class t_mat:
         V_mat = UIB * self.vertices.V_mat()
         W_mat = UIB * self.vertices.W_mat()
 
-        PI1100 = np.divide(U_mat, Den1)
-        PI1121[indk, indq] = coeff * (Uelem / Den22)
-        PI21[indk, indq] = coeff * (Welem / Den22)
-        PI2200[indk, indq] = coeff * (Welem / Den22)
-        PI12[indk, indq] = coeff * (Uelem / Den)
-        PI22[indk, indq] = coeff * (Welem / Den)
+        Pair_Prop_11_12 = np.divide(U_mat, Den1)
+        Pair_Prop_21_22 = np.divide(W_mat, Den1)
+        Pair_Prop_12_SE = np.divide(U_mat, Den2)
+        Pair_Prop_22_SE = np.divide(W_mat, Den2)
 
-        PI1100 = PI1121 = PI21 = PI12 = PI2200 = PI22 = inv11 = inv12 = inv22 = T1100 = T1200 = T1121 = T1222 = T2100 = T2200 = T12 = T22 = np.zeros((dim, dim), dtype=complex)
-        indk0 = 0
-        indq0 = 0
-        ZeroIndx1 = True
-        ZeroIndx2 = True
-
-        for lambda_ in range(cutoff):
-            for lambda1 in range(cutoff):
-                for kx1 in range(Lx):
-                    for ky1 in range(Ly):
-                        for qx1 in range(Lx):
-                            for qy1 in range(Ly):
-                                kx = kx1
-                                ky = ky1
-                                qx = qx1
-                                qy = qy1
-                                indk = lambda_ * Lx * Lx + kx * Ly + ky
-                                indq = lambda1 * Ly * Ly + qx * Ly + qy
-                                if lambda_ == 0 and KXs[kx] == 0 and KYs[ky] == 0 and ZeroIndx1 == True:
-                                    indk0 = indk
-                                    ZeroIndx1 = False
-                                if lambda1 == 0 and KXs[qx] == 0 and KYs[qy] == 0 and ZeroIndx1 == True:
-                                    indq0 = indq
-                                    ZeroIndx2 = False
-                                Uelem = UIB * self.vertices.U(kx, ky, lambda_, qx, qy, lambda1)
-                                Velem = UIB * self.vertices.V(kx, ky, lambda_, qx, qy, lambda1)
-                                Welem = UIB * (self.vertices.W(kx, ky, lambda_, qx, qy, lambda1) + self.vertices.W(qx, qy, lambda1, kx, ky, lambda_))
-                                epsplus = self.epsI(KXs[kx] + KXs[qx], KXs[ky] + KXs[qy])
-                                epsminus = self.epsI(KXs[kx] - KXs[qx], KXs[ky] - KXs[qy])
-
-                                if lambda1 == 0:
-                                    epsplus = self.epsI(KXs[kx], KXs[ky])
-                                    epsminus = self.epsI(KXs[kx], KXs[ky])
-                                if lambda_ == 0:
-                                    epsplus = self.epsI(KXs[qx], KXs[qy])
-                                    epsminus = self.epsI(-KXs[qx], -KXs[qy])
-                                Den11 = self.Epol - omegaklambda[lambda1, qx, qy] - dJU * self.epsI(KXs[qx], KYs[qy]) + eta * 1j
-                                Den22 = self.Epol - omegaklambda[lambda1, qx, qy] - dJU * self.epsI(KXs[qx], KYs[qy]) + eta * 1j
-                                Den = self.Epol - omegaklambda[lambda_, kx, ky] - omegaklambda[lambda1, qx, qy] - dJU * epsplus + eta * 1j
-                                Us[indk, indq] = Uelem
-                                Vs[indk, indq] = Velem
-                                Ws[indk, indq] = Welem
-                                coeff = 1 / (4 * np.pi * np.pi) * dkxs[qx] * dkys[qy]
-                                PI1100[indk, indq] = coeff * (Uelem / Den11)
-                                PI1121[indk, indq] = coeff * (Uelem / Den22)
-                                PI21[indk, indq] = coeff * (Welem / Den22)
-                                PI2200[indk, indq] = coeff * (Welem / Den22)
-                                PI12[indk, indq] = coeff * (Uelem / Den)
-                                PI22[indk, indq] = coeff * (Welem / Den)
-                                if lambda1 == 0:
-                                    PI1100[indk, indq] = 0
-                                    PI1121[indk, indq] = 0
-                                    PI21[indk, indq] = 0
-                                    PI12[indk, indq] = 0
-                                    PI2200[indk, indq] = 0
-                                    PI22[indk, indq] = 0
         IMat = np.eye(dim)
-        inv11 = inv(IMat - PI1100)
-        inv22 = inv(IMat - PI1121)
-        T1100 = inv11 @ Us
-        T1200 = inv11 @ Ws
-        T1121 = inv22 @ Us
-        T2100 = Ws + PI21 @ T1121
-        T1222 = inv22 @ Ws
-        T2200 = Vs + PI21 @ T1222
-        inv12 = inv(IMat - PI12)
-        T12 = inv12 @ Ws
-        T22 = Vs + 0.5 * PI22 @ T12
-        T22diag = np.diag(T22)
+
+        # Dividing over M missing
+        inv11 = inv(IMat - Pair_Prop_11_12)
+        T_11 = inv11 @ U_mat
+        T_12 = inv11 @ W_mat
+        T_21 = W_mat + Pair_Prop_21_22 @ T_11
+        T_22 = V_mat + Pair_Prop_21_22 @ T_12
+
+        inv12 = inv(IMat - Pair_Prop_12_SE)
+        T_12_SE = inv12 @ W_mat
+        T_22_SE = V_mat + 0.5 * Pair_Prop_22_SE @ T_12_SE
+
+        T22diag = np.diag(T_22_SE)
         for lambda_ in range(1, cutoff + 1):
             for kx in range(M):
                 for ky in range(M):
                     indk = lambda_ * M * M + kx * M + ky
-                    Sigma22 += 1.0 / (4 * np.pi * np.pi) * dkxs[kx] * dkxs[ky] * T22diag[indk]
-        sigpol = T1100[indk0, indq0] + T1200[indk0, indq0] + T2100[indk0, indq0] + T2200[indk0, indq0] + Sigma22
-        return np.array([np.real(Epol - sigpol), T1100[indk0, indq0], T12[indk0, indq0], T2100[indk0, indq0], T2200[indk0, indq0], Sigma22, sigpol], dtype=np.complex128)
+                    Sigma22 += T22diag[indk]
+        indk0 = 0
+        indq0 = 0
+        sigpol = T_11[indk0, indq0] + T_12[indk0, indq0] + T_21[indk0, indq0] + T_22[indk0, indq0] + Sigma22
 
     def SigmaPolaron(self):
         cutoff = self.cutoff
